cd371f86b5aab7126e6b244694e40f8fd5b797ce
Dibash <desktop555555874@gmail.com>
2026-01-07 19:36:51 +0545

    Update solutions1.js

--- javascriptlearning/beginnerQuestionswith Answers/solutions1.js
+++ javascriptlearning/beginnerQuestionswith Answers/solutions1.js
@@ -7,11 +7,15 @@ function showtext() {
 
     // show in console
     console.log(name);
-
+if(name === "") {
+    result.innerHTML="PLease enter name";
+    return;
+}
     //Display in page
  if(result) {
      result.innerHTML=`you printed ${name}`;
  }
+
 
 
 }
