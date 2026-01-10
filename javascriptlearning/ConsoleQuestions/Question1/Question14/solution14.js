b501e4cdfad4396821a06e560b6b7722c6219fd3
Dibash <desktop555555874@gmail.com>
2026-01-10 20:29:39 +0545

    Update solution14.js

--- javascriptlearning/ConsoleQuestions/Question1/Question14/solution14.js
+++ javascriptlearning/ConsoleQuestions/Question1/Question14/solution14.js
@@ -7,19 +7,19 @@ grounds= true;
 fisrtaid=true;
 
 for(let i=0; i<teachers.length; i++) {
-    if(teachers[i]===true){
+    if(teachers[i]===true)
         teacherCount++;
-    }
-    for(let j=0; j<students.length; j++) {
-        if(students[j]===true) {
-            StudentsCount++;
-        }
-    }
 }
+    for(let j=0; j<students.length; j++) {
+        if(students[j]===true) 
+            StudentsCount++;
+        
+    }
 let sportsevent= (teacherCount>=3) && (StudentsCount>=5) && grounds===true &&fisrtaid===true;
 
 if(sportsevent) {
     console.log("Sports Event Started");
 } else {
     console.log("Sports Event Canceled");
+
 }
