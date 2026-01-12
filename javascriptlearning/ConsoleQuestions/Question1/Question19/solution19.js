1f428366509e9cd60d5dae2533d7070e4a9957bc
Dibash <desktop555555874@gmail.com>
2026-01-12 20:48:59 +0545

    Add files via upload

--- javascriptlearning/ConsoleQuestions/Question1/Question19/solution19.js
+++ javascriptlearning/ConsoleQuestions/Question1/Question19/solution19.js
@@ -1,6 +1,14 @@
-let electricity= true;
-if(electricity=== true) {
-    console.log("works");
+let switches =[true,false,false];
+let light =false;
+
+for(let i=0; i<switches.length; i++) {
+    if(switches[i]===true) {
+           light= true;
+           break;
+    }
+}
+if(light) {
+    console.log("The light is ON");
 } else {
-    console.log("Fail");
+    console.log("The light is OFF");
 }
