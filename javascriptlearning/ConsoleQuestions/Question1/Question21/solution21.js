da91a099317f38fa6e541d776ee03de8db20e982
Dibash <desktop555555874@gmail.com>
2026-01-13 20:26:36 +0545

    Add files via upload

---
+++ javascriptlearning/ConsoleQuestions/Question1/Question21/solution21.js
@@ -0,0 +1,8 @@
+let batteryPresent = true;
+let powerConnected = true;
+
+if (batteryPresent && powerConnected) {
+    console.log("Laptop Started");
+} else {
+    console.log("Laptop Not Started");
+}
