c617c2e4554897bb0e73373f55a6c37527b1432d
Dibash <desktop555555874@gmail.com>
2026-01-13 20:37:57 +0545

    Add files via upload

---
+++ javascriptlearning/ConsoleQuestions/Question1/Question23/solution23.js
@@ -0,0 +1,8 @@
+let staffPresent = true;
+let electricityAvailable = true;
+
+if (staffPresent && electricityAvailable) {
+    console.log("Bank Open");
+} else {
+    console.log("Bank Closed");
+}
