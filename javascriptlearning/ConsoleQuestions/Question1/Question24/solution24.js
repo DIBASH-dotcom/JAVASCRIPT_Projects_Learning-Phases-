053f4b14098291392176246e5bc0875f99e7c277
Dibash <desktop555555874@gmail.com>
2026-01-13 20:44:38 +0545

    Add files via upload

---
+++ javascriptlearning/ConsoleQuestions/Question1/Question24/solution24.js
@@ -0,0 +1,8 @@
+let projectorWorking = true;
+let soundSystemWorking = true;
+
+if (projectorWorking && soundSystemWorking) {
+    console.log("Movie Playing");
+} else {
+    console.log("Movie Cannot Play");
+}
