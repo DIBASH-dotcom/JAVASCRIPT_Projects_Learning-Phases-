c9c70690c3d062512d577cc6ec614154ab6ec6b2
Dibash <desktop555555874@gmail.com>
2026-01-14 12:26:16 +0545

    Add files via upload

---
+++ javascriptlearning/ConsoleQuestions/Question1/Question25/solution25.js
@@ -0,0 +1,8 @@
+let smokeSeen = true;
+let fireSeen = false;
+
+if (smokeSeen || fireSeen) { //  this sigh means (||)=एउटा भए पनि true hunuparxa
+    console.log("Alarm Ring");
+} else {
+    console.log("No Alarm");
+}
