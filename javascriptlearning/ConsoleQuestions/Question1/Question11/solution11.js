12ecec74de93671980c723068d1cd6a26a678d25
Dibash <desktop555555874@gmail.com>
2026-01-19 21:52:37 +0545

    Add hospital availability check and doctor status logging

--- javascriptlearning/ConsoleQuestions/Question1/Question11/solution11.js
+++ javascriptlearning/ConsoleQuestions/Question1/Question11/solution11.js
@@ -1,19 +1,50 @@
-let students=[true,true, true,true, true,true,true,true,true,true];
-let count =0;
-let librarian =true;
-let  electricity= true;
+let  doctors=["Dibash Magar","Dilip Khattri","Ravi Lamichnee", "Shyam Magar"];
+let isAvailability=[true,true,true,true];
 
-for(let i=0; i<students.length; i++) {
-    if(students[i] ===true) {
-        count ++;
+let doctorsCount=0;
+
+// count  present doctors
+for(let i=0; i<isAvailability.length; i++) {
+    if(isAvailability[i]) {
+        doctorsCount++;
+
     }
 }
-let libraryOpen = count >= 10 && librarian === true && electricity === true;
 
-if(libraryOpen) {
-    console.log("Libbary is Open ")
-} 
-else {
-    console.log("someone is missing student,electrcity or libraran");
-}
+////decsion
+ if(doctorsCount>=2) {
+    console.log("hospital open");
+ }  else {
+    console.log("hospital closed");
+ }
 
+ //print the doctorName and there  status
+
+ let statusDoctor=0;
+ while(statusDoctor<doctors.length) {
+    console.log(doctors[statusDoctor]+":is"+(isAvailability[statusDoctor]? "available":"unavailable"));
+    statusDoctor++;
+ }
+
+ // check Avalilability of doctors
+
+ let j=0;
+ do {
+    console.log("Availability checked for"+doctors[j]);
+    j++;
+ } while(j<doctors.length);
+
+ //// switch condition
+
+ switch(doctorsCount){
+     case doctors.length: // if all true
+        console.log("All doctors available");
+        break;
+    case 2:
+    case 3:
+        console.log("Minimum doctors available");
+        break;
+    default:
+        console.log("Hospital closed");
+
+ }
