a64046a15ad2d7d608dc74a7c35a939065e25add
Dibash <desktop555555874@gmail.com>
2026-01-21 21:04:11 +0545

    Delete javascriptlearning/ConsoleQuestions/Question1/Question24 directory

--- javascriptlearning/ConsoleQuestions/Question1/Question24/solution24.js
+++
@@ -1,8 +0,0 @@
-let projectorWorking = true;
-let soundSystemWorking = true;
-
-if (projectorWorking && soundSystemWorking) {
-    console.log("Movie Playing");
-} else {
-    console.log("Movie Cannot Play");
-}
