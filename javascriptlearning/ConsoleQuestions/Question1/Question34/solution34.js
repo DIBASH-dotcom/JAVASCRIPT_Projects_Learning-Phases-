de6f3d5465206657cc3753b86eb9cae5ed200f23
Dibash <desktop555555874@gmail.com>
2026-01-21 21:12:32 +0545

    Delete javascriptlearning/ConsoleQuestions/Question1/Question34 directory

--- javascriptlearning/ConsoleQuestions/Question1/Question34/solution34.js
+++
@@ -1,9 +0,0 @@
-let  onlineUsers =[true,true,true,false];
-let onlineUsersCount =0;
-
-for(let i=0; i<onlineUsers.length; i++) {
-    if(onlineUsers[i]) {
-        onlineUsersCount++;
-    }
-}
-console.log("Number of Online Users:", onlineUsersCount);
