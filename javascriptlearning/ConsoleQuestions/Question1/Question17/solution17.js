08568952ed49ae97709fdc5617fadb7429251473
Dibash <desktop555555874@gmail.com>
2026-01-21 20:15:06 +0545

    Enhance score validation and output logic

--- javascriptlearning/ConsoleQuestions/Question1/Question17/solution17.js
+++ javascriptlearning/ConsoleQuestions/Question1/Question17/solution17.js
@@ -1,5 +1,8 @@
-let score= 40;
-if(score >=40) {
+let score = 40;
+
+if (score < 0 || score > 100) {
+    console.log("Invalid score");
+} else if (score >= 40) {
     console.log("Pass");
 } else {
     console.log("Fail");
