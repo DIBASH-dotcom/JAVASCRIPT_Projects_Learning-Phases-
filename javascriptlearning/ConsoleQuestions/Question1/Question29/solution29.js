30ea765f642d46129825a766108c686e27e17f29
Dibash <desktop555555874@gmail.com>
2026-01-21 21:11:02 +0545

    Delete javascriptlearning/ConsoleQuestions/Question1/Question29 directory

--- javascriptlearning/ConsoleQuestions/Question1/Question29/solution29.js
+++
@@ -1,9 +0,0 @@
-let hasGenerator = false;
-let hasInverter = true;
-let hasSolar = true;
-
-if ((hasGenerator && hasInverter) || hasSolar) {
-    console.log("Backup works");
-} else {
-    console.log("No Backup works");
-}
