96646d5a09d37d18a5c1e651bd90fe84234d7096
Dibash <desktop555555874@gmail.com>
2026-01-21 21:03:55 +0545

    Delete javascriptlearning/ConsoleQuestions/Question1/Question23 directory

--- javascriptlearning/ConsoleQuestions/Question1/Question23/solution23.js
+++
@@ -1,8 +0,0 @@
-let staffPresent = true;
-let electricityAvailable = true;
-
-if (staffPresent && electricityAvailable) {
-    console.log("Bank Open");
-} else {
-    console.log("Bank Closed");
-}
