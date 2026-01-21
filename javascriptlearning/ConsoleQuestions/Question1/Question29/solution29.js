8a80c1d48a1426235f1581ef970c422b4dbec540
Dibash <desktop555555874@gmail.com>
2026-01-21 20:43:06 +0545

    Add solar condition to backup check

--- javascriptlearning/ConsoleQuestions/Question1/Question29/solution29.js
+++ javascriptlearning/ConsoleQuestions/Question1/Question29/solution29.js
@@ -1,7 +1,8 @@
 let hasGenerator = false;
 let hasInverter = true;
+let hasSolar = true;
 
-if (hasGenerator || hasInverter) {
+if ((hasGenerator && hasInverter) || hasSolar) {
     console.log("Backup works");
 } else {
     console.log("No Backup works");
