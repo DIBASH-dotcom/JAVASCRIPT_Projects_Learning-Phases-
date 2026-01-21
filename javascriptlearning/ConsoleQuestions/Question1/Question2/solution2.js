4209bbec60869cbc40b40e2d7973a8d62b5e81ee
Dibash <desktop555555874@gmail.com>
2026-01-21 20:22:57 +0545

    Add validation for marks input in solution2.js

--- javascriptlearning/ConsoleQuestions/Question1/Question2/solution2.js
+++ javascriptlearning/ConsoleQuestions/Question1/Question2/solution2.js
@@ -1,9 +1,12 @@
 let marks=60;
-
-if(marks <40) {
+ if(marks <0 || marks>100) {
+     console.log("Invalid marks:");
+ }
+else if(marks <40) {
     console.log("Fail");
 }
 
 else {
     console.log("pass");
+
 }
