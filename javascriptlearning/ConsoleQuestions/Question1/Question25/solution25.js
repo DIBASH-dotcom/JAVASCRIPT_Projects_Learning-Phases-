a0b56f8acf04dc2f69b7a1c04e1762cb2ab10c3b
Dibash <desktop555555874@gmail.com>
2026-01-21 21:04:25 +0545

    Delete javascriptlearning/ConsoleQuestions/Question1/Question25 directory

--- javascriptlearning/ConsoleQuestions/Question1/Question25/solution25.js
+++
@@ -1,8 +0,0 @@
-let smokeSeen = true;
-let fireSeen = false;
-
-if (smokeSeen || fireSeen) { //  this sigh means (||)=एउटा भए पनि true hunuparxa
-    console.log("Alarm Ring");
-} else {
-    console.log("No Alarm");
-}
