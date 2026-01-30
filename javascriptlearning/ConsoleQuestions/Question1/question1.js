a770834ff939c8d1b01102d1d2a839d2c0aa579a
Dibash <desktop555555874@gmail.com>
2026-01-30 22:13:34 +0545

    Update question1.js

--- javascriptlearning/ConsoleQuestions/Question1/question1.js
+++ javascriptlearning/ConsoleQuestions/Question1/question1.js
@@ -1,9 +1,18 @@
-let roomOccupied = true; 
-let lightOn = true;  
-let visibility = false;
+let arr1 = [3, 4, 5, 6, 7];
+let arr2 = [1, 5, 7, 8, 7];
 
-if (roomOccupied && lightOn) {
-    visibility = true;
+let hasCommon = false;
+
+for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+        if (arr1[i] === arr2[j]) {
+            hasCommon = true;
+            break; // stop inner loop
+        }
+    }
+    if (hasCommon) {
+        break; // stop outer loop
+    }
 }
 
-console.log("Visibility On:", visibility);
+console.log(hasCommon);
