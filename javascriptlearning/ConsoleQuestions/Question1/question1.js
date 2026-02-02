b66de6f3923dbf67d0bd96eba5f1f437ad7a83ac
Dibash <desktop555555874@gmail.com>
2026-02-02 10:47:34 +0545

    Update question1.js

--- javascriptlearning/ConsoleQuestions/Question1/question1.js
+++ javascriptlearning/ConsoleQuestions/Question1/question1.js
@@ -1,18 +1,14 @@
 let arr1 = [3, 4, 5, 6, 7];
 let arr2 = [1, 5, 7, 8, 7];
 
-let hasCommon = false;
+let count = 0;
 
 for (let i = 0; i < arr1.length; i++) {
     for (let j = 0; j < arr2.length; j++) {
         if (arr1[i] === arr2[j]) {
-            hasCommon = true;
-            break; // stop inner loop
+            count++;
         }
-    }
-    if (hasCommon) {
-        break; // stop outer loop
     }
 }
 
-console.log(hasCommon);
+console.log(count);
