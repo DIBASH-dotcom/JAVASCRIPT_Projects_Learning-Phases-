f02b18b3ccc583cf5bceab54c15b4ee9c97abe55
Dibash <desktop555555874@gmail.com>
2026-02-07 20:04:32 +0545

    Update question1.js

--- javascriptlearning/ConsoleQuestions/Question1/question1.js
+++ javascriptlearning/ConsoleQuestions/Question1/question1.js
@@ -1,14 +1,5 @@
-let arr1 = [3, 4, 5, 6, 7];
-let arr2 = [1, 5, 7, 8, 7];
-
-let count = 0;
-
-for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-        if (arr1[i] === arr2[j]) {
-            count++;
-        }
-    }
+function createHelloWorld() {
+    return function () {
+        return "Hello World";
+    };
 }
-
-console.log(count);
